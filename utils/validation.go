0377131bf303a933f16a387a5efdefdd84cba1e3
Aarishch5 <chaudharyaarish417@gmail.com>
2026-07-17 05:05:08 +0530

    added the password validation

--- utils/validation.go
+++ utils/validation.go
@@ -6,8 +6,12 @@ import (
 )
 
 var (
-	number   = regexp.MustCompile(`^[0-9]+$`)
-	emailExp = regexp.MustCompile(`^[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}$`)
+	upperCase      = regexp.MustCompile(`[A-Z]`)
+	lowerCase      = regexp.MustCompile(`[a-z]`)
+	numberPassword = regexp.MustCompile(`[0-9]`)
+	special        = regexp.MustCompile(`[^a-zA-Z0-9]`)
+	number         = regexp.MustCompile(`^[0-9]+$`)
+	emailExp       = regexp.MustCompile(`^[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}$`)
 )
 
 func ValidateEmail(email string) error {
@@ -28,5 +32,22 @@ func ValidatePassword(password string) error {
 	if len(password) < 8 {
 		return errors.New("password must be at least 8 characters long")
 	}
+
+	if !upperCase.MatchString(password) {
+		return errors.New("password must contain at least one uppercase letter")
+	}
+
+	if !lowerCase.MatchString(password) {
+		return errors.New("password must contain at least one lowercase letter")
+	}
+
+	if !numberPassword.MatchString(password) {
+		return errors.New("password must contain at least one number")
+	}
+
+	if !special.MatchString(password) {
+		return errors.New("password must contain at least one special character")
+	}
+
 	return nil
 }
